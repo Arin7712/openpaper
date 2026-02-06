7efba8a5dbfffa7297a623e04b846260dcea074a
Arin7712 <aringawande7712@gmail.com>
2026-02-06 12:07:16 +0530

    fixed upload-form validations

--- lib/validations/upload-form.ts
+++ lib/validations/upload-form.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
-export const UploadFormSchema = {
+export const UploadFormSchema = z.object({
     title: z.string().min(5, "Title must be atleast 5 characters long."),
     abstract: z.string().min(10, "Abstract must be atleast 10 characters long."),
     pdfUrl: z.string().url("PDF URL must be a valid URL."),
-}
+})
