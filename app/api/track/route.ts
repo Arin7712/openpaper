cc49dedfa927d273912c6eeeef475f1ab6495ad5
Arin7712 <aringawande7712@gmail.com>
2026-02-23 16:02:15 +0530

    ok

--- app/api/track/route.ts
+++ app/api/track/route.ts
@@ -3,12 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
 
     const body = await req.json();
+
+    const country =
+        req.headers.get("x-vercel-ip-country") ||
+        req.headers.get("x-forwarded-for-country") || // fallback if proxy used
+        "unknown";
+
     const log = {
         source: body.source || "direct",
-        userAgent:req.headers.get("user-agent"),
+        country,
+        userAgent: req.headers.get("user-agent"),
         time: new Date().toISOString()
     };
 
     console.log("VISIT:", log);
+
     return NextResponse.json({ success: true });
 }
